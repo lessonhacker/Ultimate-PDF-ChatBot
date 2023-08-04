b8336e5c629adc9b769d05706235dd8fcedd52de
lessonhacker <admin@lessonhacker.com>
2023-08-04 10:24:51 +0000

    Changed to chevron and elipses

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ file_list = os.listdir("docs")
 
 while True:
   os.system("clear")
-  input_query = input("\nAsk your question: ")
+  input_query = input("\nAsk your question> ")
   answer = chat_with_web_app.query(input_query)
   print(f"Answer: {answer}\n\n")
-  input("Press any key to ask another question: ")
+  input("Press any key to ask another question… ")
